56c8ce790547f88eefab63df432bf73cc27b6e7e
heykapil <101615305+heykapil@users.noreply.github.com>
2022-04-29 02:07:12 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -10,14 +10,14 @@ export default function Footer() {
         <div className="flex mb-3 space-x-4">
           <SocialIcon kind="github" href={siteMetadata.github} />
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} />
-          <SocialIcon kind="facebook" href={`mailto:${siteMetadata.facebook}`} />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} />
           <SocialIcon kind="instagram" href={siteMetadata.instagram} />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} />
         </div>
         <div className="background-author-animate mb-2 flex space-x-2 bg-gradient-to-r from-red-500 via-purple-500 to-yellow-500 bg-clip-text text-base font-semibold text-transparent">
-          <div>{siteMetadata.author} | Have a good {currentDayName()}!</div>
+          <div>Have a good {currentDayName()}!</div>
         </div>
         <div className="mb-3 flex">
           <NowPlaying />
