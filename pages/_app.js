29c521c2ac49aa08b95256904c9d866c2f66fca7
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-04-29 23:21:24 +0530

    nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,28 @@ import '@/css/twemoji.css'
 import '@/css/timeline.css'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
+import Router from 'next/router';
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import Extra from '@/components/Extra'
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: true });
+
+Router.onRouteChangeStart = () => {
+  // console.log('onRouteChangeStart triggered');
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  // console.log('onRouteChangeComplete triggered');
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  // console.log('onRouteChangeError triggered');
+  NProgress.done();
+};
 
 export default function App({ Component, pageProps }) {
   return (
