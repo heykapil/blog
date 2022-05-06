ca17d5c905e08341aae0c9f481f5f7ff68654773
heykapil <101615305+heykapil@users.noreply.github.com>
2022-05-07 00:09:21 +0530

    Create viewer.js

---
+++ pages/viewer.js
@@ -0,0 +1,15 @@
+import Script from 'next/script'
+export default function Viewer() {
+  return (
+    <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
+    <Script type="text/javascript">
+	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+		var adobeDCView = new AdobeDC.View({clientId: "432a6f34caa24103815ebb8b12751934"});
+		adobeDCView.previewFile({
+			content:{location: {url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
+			metaData:{fileName: "Bodea Brochure.pdf"}
+		}, {embedMode: "LIGHT_BOX"});
+	});
+    </Script>
+  )
+}
