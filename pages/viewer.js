5a911cb26d347eb425391833e27979462333ea78
heykapil <101615305+heykapil@users.noreply.github.com>
2022-05-07 00:21:04 +0530

    Update viewer.js

--- pages/viewer.js
+++ pages/viewer.js
@@ -2,7 +2,7 @@ import Script from 'next/script'
 export default function Viewer() {
   return (
     <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
-    <Script>
+    <Script dangerouslySetInnerHTML={{ __html: `
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
 		var adobeDCView = new AdobeDC.View({clientId: "432a6f34caa24103815ebb8b12751934"});
 		adobeDCView.previewFile({
@@ -10,6 +10,6 @@ export default function Viewer() {
 			metaData:{fileName: "Bodea Brochure.pdf"}
 		}, {embedMode: "LIGHT_BOX"});
 	});
-    </Script>
+    `}} />
   )
 }
