df82ccd2ce2d5a4451c2a20d2d86700811de2977
heykapil <101615305+heykapil@users.noreply.github.com>
2022-05-07 00:14:58 +0530

    Update viewer.js

--- pages/viewer.js
+++ pages/viewer.js
@@ -2,7 +2,7 @@ import Script from 'next/script'
 export default function Viewer() {
   return (
     <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
-    <Script type="text/javascript">
+    <Script>
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
 		var adobeDCView = new AdobeDC.View({clientId: "432a6f34caa24103815ebb8b12751934"});
 		adobeDCView.previewFile({
