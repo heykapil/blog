4560fc78ac60995cee18bb0cbc75d2059cbeb0bb
heykapil <101615305+heykapil@users.noreply.github.com>
2022-05-07 00:48:57 +0530

    Update viewer.js

--- pages/viewer.js
+++ pages/viewer.js
@@ -2,7 +2,7 @@ import Script from 'next/script'
 export default function Viewer() {
   return (
     <div id="adobe-dc-view"></div>
-    <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
-    <Script src="./static/view.js" />
+    <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+    <script type="text/javascript" src="./static/view.js"></script>
   )
 }
