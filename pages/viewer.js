7b3b90a3c654b13c07008db649a4a5dc452a7b62
heykapil <101615305+heykapil@users.noreply.github.com>
2022-05-07 00:31:03 +0530

    Update viewer.js

--- pages/viewer.js
+++ pages/viewer.js
@@ -3,18 +3,17 @@ export default function Viewer() {
   return (
     <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
     <Script
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-    (document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-		var adobeDCView = new AdobeDC.View({clientId: "432a6f34caa24103815ebb8b12751934", divId: "adobe-dc-view"});
+      dangerouslySetInnerHTML={{
+      __html: `
+     document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+ 		var adobeDCView = new AdobeDC.View({clientId: "432a6f34caa24103815ebb8b12751934", divId: "adobe-dc-view"});
 		adobeDCView.previewFile({
 			content:{location: {url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
 			metaData:{fileName: "Bodea Brochure.pdf"}
 		}, {});
-	}););
-  `,
-  }}
-/>
+	});
+     `,
+     }}
+     />
   )
 }
