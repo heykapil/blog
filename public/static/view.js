ce7660fe18c5ef482b1628356a34597b9b859cf6
heykapil <101615305+heykapil@users.noreply.github.com>
2022-05-07 00:40:44 +0530

    Create view.js

---
+++ public/static/view.js
@@ -0,0 +1,7 @@
+document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+		var adobeDCView = new AdobeDC.View({clientId: "432a6f34caa24103815ebb8b12751934", divId: "adobe-dc-view"});
+		adobeDCView.previewFile({
+			content:{location: {url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
+			metaData:{fileName: "Bodea Brochure.pdf"}
+		}, {});
+	});
