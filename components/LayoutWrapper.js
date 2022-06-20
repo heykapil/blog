ff1ab675dd838fc31c55b4d38cd1c5f9b87b028b
heykapil <101615305+heykapil@users.noreply.github.com>
2022-06-20 17:31:57 +0530

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -4,12 +4,20 @@ import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import NextImage from 'next/image'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const LayoutWrapper = ({ children }) => {
   const [navShow, setNavShow] = useState(false)
-  const onToggleNav = () => setNavShow((status) => !status)
-
+  const onToggleNav = () => {
+    setNavShow((status) => {
+      if (status) {
+        document.body.style.overflow = 'auto'
+      } else {
+        document.body.style.overflow = 'hidden'
+      }
+      return !status
+    })
+  }
   return (
     <>
       <MobileNav navShow={navShow} onToggleNav={onToggleNav} />
@@ -42,9 +50,10 @@ const LayoutWrapper = ({ children }) => {
               ))}
             </div>
             <ThemeSwitch />
+            <div className="sm:hidden">
             <button
-              className="w-8 h-8 ml-2 mr-1 rounded sm:hidden"
               type="button"
+              className="ml-1 mr-1 h-8 w-8 rounded"
               aria-label="Toggle Menu"
               onClick={onToggleNav}
             >
@@ -52,15 +61,24 @@ const LayoutWrapper = ({ children }) => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
-                className="text-gray-900 dark:text-gray-100"
+                className="text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
+                {navShow ? (
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                ) : (
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  />
+                )}
               </svg>
             </button>
+          </div>
           </div>
         </div>
       </header>
