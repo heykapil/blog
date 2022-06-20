0eb2a599d881cd8f43f3858ad92135c4b2540cfb
heykapil <101615305+heykapil@users.noreply.github.com>
2022-06-20 16:55:00 +0530

    Update MobileNav.js

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -5,36 +5,32 @@ const MobileNav = ({ navShow, onToggleNav }) => {
 
   return (
     <div className="sm:hidden">
-     <button
-        type="button"
-        className="ml-1 mr-1 h-8 w-8 rounded py-1"
-        aria-label="Toggle Menu"
-        onClick={onToggleNav}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="text-gray-900 dark:text-gray-100"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
       <div
         className={`fixed w-full h-screen top-12 right-0  z-40 ease-in-out duration-500 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         } bg-opacity-30 dark:bg-opacity-30 backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100`}
       >
-        <button
-          type="button"
-          aria-label="toggle modal"
-          className="fixed z-30 w-full h-full cursor-auto focus:outline-none "
-          onClick={onToggleNav}
-        ></button>
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="mr-5 mt-11 h-8 w-8 rounded"
+            aria-label="Toggle Menu"
+            onClick={onToggleNav}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="text-gray-900 dark:text-gray-100"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
         <nav className="mt-8 relative space-y-8 z-40">
           {headerNavLinks.map((link) => (
             <div key={link.title} className="px-12">
