1f05accb8c59b80e0a45442663d3a317ec11fb69
heykapil <101615305+heykapil@users.noreply.github.com>
2022-06-20 19:27:16 +0530

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -44,7 +44,7 @@ const LayoutWrapper = ({ children }) => {
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100 link-underline link-underline-black"
                 >
                   {link.title}
                 </Link>
