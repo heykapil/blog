4eebc3f1eb6e9dcafe891db2ca60a2a3ecf42297
heykapil <101615305+heykapil@users.noreply.github.com>
2022-06-20 18:40:39 +0530

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -24,7 +24,7 @@ const LayoutWrapper = ({ children }) => {
         className={`fixed w-full bg-transparent top-0 z-40 flex items-center justify-between bg-white bg-opacity-30 backdrop-blur-lg firefox:bg-opacity-100 dark:bg-opacity-30 dark:firefox:bg-opacity-100 overflow-x-hidden backdrop-blur supports-backdrop-blur:bg-white/95 py-3 sticky`}
       >
         <nav className="mx-auto max-w-3xl xl:max-w-5xl flex items-center justify-between px-3 xl:px-0">
-          <div className="mx-auto max-w-3xl xl:max-w-5xl flex items-center justify-between px-3 xl:px-0>
+          <div>
             <Link href="/" aria-label="hello kapil">
               <div className="flex items-center justify-between">
                 <div className="mr-3 flex justify-center items-center">
