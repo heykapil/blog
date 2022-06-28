f9b71031c00dfaf413df8061dd1c8e99f77d0daf
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-06-28 15:49:55 +0530

    small-fixes

--- pages/index.js
+++ pages/index.js
@@ -177,31 +177,11 @@ export default function Home({ posts }) {
                         </div>
                       </div>
                     </div>
-                    <div className="mt-3 flex mx-auto gap-12 content-start">
-                      <div className="capsize flex items-center text-gray-800 dark:text-gray-200">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="mr-2 h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                          ></path>
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                          ></path>
-                        </svg>
-                        {readTime}
+                    <div className="mt-4 flex">
+                      <div className="capsize text-gray-800 dark:text-gray-200">
+                          {readTime}
                       </div>
-                      <div className="text-base align-right font-medium leading-6 content-end">
+                      <div className="text-base ml-auto font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 link-underline link-underline-black"
