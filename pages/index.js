cf96a6cfdb480c49b91aec75515492a52a510389
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-06-28 16:46:48 +0530

    timer-svg-icon-reading-time

--- pages/index.js
+++ pages/index.js
@@ -178,10 +178,12 @@ export default function Home({ posts }) {
                       </div>
                     </div>
                     <div className="mt-4 flex">
-                      <div className="capsize text-gray-800 dark:text-gray-200">
-                          {readTime}
-                      </div>
-                      <div className="text-base ml-auto font-medium leading-6">
+                      <div className="text-gray-800  dark:text-gray-200"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+width="20" height="20"
+viewBox="0 0 30 30"
+fill="currentColor"><path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M16,16H7.995 C7.445,16,7,15.555,7,15.005v-0.011C7,14.445,7.445,14,7.995,14H14V5.995C14,5.445,14.445,5,14.995,5h0.011 C15.555,5,16,5.445,16,5.995V16z"></path></svg>
+</div><div className='mr-auto'>{readTime}</div>
+                      <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 link-underline link-underline-black"
