6b1c2695bbc07172f94e27f8c04a7bc0a4850c7d
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2022-07-02 10:25:54 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ import Extra from '@/components/Extra'
 import NProgress from 'nprogress';
 import { Provider } from 'next-auth/client'
 NProgress.configure({ showSpinner: true });
-
+import React, { useState, useEffect } from 'react';
 Router.onRouteChangeStart = () => {
   // console.log('onRouteChangeStart triggered');
   NProgress.start();
@@ -31,25 +31,12 @@ Router.onRouteChangeError = () => {
 
 export default function App({ Component, pageProps }) {
     return ( 
-      <Provider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      options={{
-        // Client Max Age controls how often the useSession in the client should
-        // contact the server to sync the session state. Value in seconds.
-        // e.g.
-        // * 0  - Disabled (always use cache value)
-        // * 60 - Sync session state with server if it's older than 60 seconds
-        clientMaxAge: 0,
-        // Keep Alive tells windows / tabs that are signed in to keep sending
-        // a keep alive request (which extends the current session expiry) to
-        // prevent sessions in open windows from expiring. Value in seconds.
-        //
-        // Note: If a session has expired when keep alive is triggered, all open
-        // windows / tabs will be updated to reflect the user is signed out.
-        keepAlive: 0
-      }}
-      session={pageProps.session} >
+      <Provider session={pageProps.session}>
+      {Component.auth ? (
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      ) : (
         <ThemeProvider attribute="class">
         <Head>
           <meta content="width=device-width, initial-scale=1" name="viewport" />
@@ -60,7 +47,25 @@ export default function App({ Component, pageProps }) {
         </LayoutWrapper>
         <Extra />
       </ThemeProvider>
-      </Provider>
+      )}
+    </Provider>
 
   )
+}
+
+function Auth({ children }) {
+  const [session, loading] = useSession()
+  const isUser = !!session?.user
+  React.useEffect(() => {
+    if (loading) return // Do nothing while loading
+    if (!isUser) signIn() // If not authenticated, force log in
+  }, [isUser, loading])
+
+  if (isUser) {
+    return children
+  }
+
+  // Session is being fetched, or no user.
+  // If no user, useEffect() will redirect.
+  return <div>Loading...</div>
 }
