115c4a4906228396fd228f2d275ea5ea685f34fc
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-07-02 23:56:15 +0530

    fresh-attempt-to-fix-guestbook

--- pages/_app.js
+++ pages/_app.js
@@ -6,44 +6,35 @@ import '@/css/twemoji.css'
 import '@/css/timeline.css'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-import Router from 'next/router';
+import Router , { useRouter } from 'next/router'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import Extra from '@/components/Extra'
 import NProgress from 'nprogress';
 import { SessionProvider } from 'next-auth/react';
 
-NProgress.configure({ showSpinner: true });
 
-Router.onRouteChangeStart = () => {
-  // console.log('onRouteChangeStart triggered');
-  NProgress.start();
-};
-
-Router.onRouteChangeComplete = () => {
-  // console.log('onRouteChangeComplete triggered');
-  NProgress.done();
-};
-
-Router.onRouteChangeError = () => {
-  // console.log('onRouteChangeError triggered');
-  NProgress.done();
-};
-
-export default function App({ Component, pageProps }) {
-    return ( 
-      <SessionProvider session={pageProps.session}>
-        <ThemeProvider attribute="class">
+function App({ Component, pageProps }) {
+  const { session, ...rest } = pageProps;
+  return (
+    <>
+      <ThemeProvider attribute="class">
+        <SessionProvider session={session}>
         <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+            />
         </Head>
         <Analytics />
         <LayoutWrapper>
-          <Component {...pageProps} />
+          <Component {...rest} />
         </LayoutWrapper>
         <Extra />
-      </ThemeProvider>
       </SessionProvider>
+      </ThemeProvider>
+    </>
 
   )
 }
+export default App;
