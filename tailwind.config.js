00ff52f3fe523dba4c83c695160ac77f6561837e
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2022-07-07 15:19:15 +0530

    minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -158,7 +158,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['GBSuperDisplay', 'IBM Plex Sans'],
+        sans: ['IBM Plex Sans','GBSuperDisplay'],
         cansu: ['Cansu'],
       },
       gradientColorStops: {
