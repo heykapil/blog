63c6cf88f175a789a73c03b7c283c9597bd7b126
Kapil Chaudhary <101615305+heykapil@users.noreply.github.com>
2022-07-10 01:03:33 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,11 @@ import Router from 'next/router';
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import Extra from '@/components/Extra'
+import { ClientReload } from '@/components/ClientReload'
 import NProgress from 'nprogress';
 import { SessionProvider } from 'next-auth/react';
-
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isSocket = process.env.SOCKET
 
 NProgress.configure({ showSpinner: true });
 
@@ -34,38 +36,21 @@ Router.onRouteChangeError = () => {
 };
 
 export default function App({ Component, pageProps: { session, ...pageProps }, }) {
-  
-  useEffect(() => {
-    renderMathInElement(document.body, {
-      delimiters: [
-        {left: "$$", right: "$$", display: true},
-        {left: "\\(", right: "\\)", display: false},
-        {left: "\\begin{equation}", right: "\\end{equation}", display: true},
-        {left: "\\begin{align}", right: "\\end{align}", display: true},
-        {left: "\\begin{alignat}", right: "\\end{alignat}", display: true},
-        {left: "\\begin{gather}", right: "\\end{gather}", display: true},
-        {left: "\\begin{CD}", right: "\\end{CD}", display: true},
-        {left: "\\[", right: "\\]", display: true},
-        {left: "$", right: "$", display: true}
-    ],
-    // rendering keys
-    throwOnError : false
-    });
-  }, [Component])
 
   return (
-    <ThemeProvider attribute="class">
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <Analytics />
-      <LayoutWrapper>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-      </LayoutWrapper>
-      <Extra />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider>    
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        {isDevelopment && isSocket && <ClientReload />}
+        <Analytics />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
+    </SessionProvider>
+     
     
   )
 }
